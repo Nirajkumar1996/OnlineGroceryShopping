5c4719dc78ee180390548d3acd28dd25b987557f
Nirajkumar Ablankar <ablankar.niraj@gmail.com>
2021-03-07 15:57:30 +0530

    Cetralize error Handling and Production Development  error separation wth middleware

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 
+const errorMiddleware = require("./middlewares/errors");
+
 app.use(express.json());
 
 //import all routes from
 const products = require("./routes/product");
 
 app.use("/api/v1", products);
+
+//make sure to configure middleware after routes
+app.use(errorMiddleware);
 
 module.exports = app;
---
+++ backend/middlewares/errors.js
@@ -0,0 +1,27 @@
+const ErrorHandler = require("../utils/errorHandler");
+
+//separate Development and Production error
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+
+  if (process.env.NODE_ENV === "DEVELOPMENT") {
+    res.status(err.statusCode).json({
+      success: false,
+      error: err,
+      errMessage: err.message,
+      stack: err.stack,
+    });
+  }
+
+  if (process.env.NODE_ENV === "PRODUCTION") {
+    //before sending error making copy of err
+    let error = { ...err };
+
+    error.message = err.message;
+
+    res.status(error.statusCode).json({
+      success: false,
+      message: error.message || "Internal Server Error",
+    });
+  }
+};
