b8cc1f28f11e8027139c57fda3e5e190d122ffd0
Nirajkumar Ablankar <ablankar.niraj@gmail.com>
2021-03-09 17:36:50 +0530

    Search Products by Keyword queryString

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,6 +2,7 @@ const Product = require("../models/product");
 
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+const APIFeatures = require("../utils/apiFeatures");
 
 //Create new product => /api/v1/admin/product/new
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
@@ -13,10 +14,11 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
-//get All products => /api/v1/products
+//get All products => /api/v1/products?keyword=somename
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
+  const apiFeatures = new APIFeatures(Product.find(), req.query).search();
   //find gives all products from database
-  const products = await Product.find();
+  const products = await apiFeatures.query;
 
   res.status(200).json({
     success: true,
