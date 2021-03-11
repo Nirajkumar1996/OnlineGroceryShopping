e05474351d492ab3280c2afa6c518c12fd076258
Nirajkumar Ablankar <ablankar.niraj@gmail.com>
2021-03-11 12:30:16 +0530

    duplicate key error, Wrong JWT and Expired JWT error handling in production mode

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -32,6 +32,24 @@ module.exports = (err, req, res, next) => {
       error = new ErrorHandler(message, 400);
     }
 
+    //Handling mongoose duplicate key errors code=11000 when duplicate key occurs mongoerror
+    if (err.code === 11000) {
+      const message = "Duplicate email entered";
+      error = new ErrorHandler(message, 400);
+    }
+
+    //Handling wrong JWT error
+    if (err.name === "JsonWebTokenError") {
+      const message = "JSON Web Token is invalid, Try again!!!";
+      error = new ErrorHandler(message, 400);
+    }
+
+    //Handling expired JWT error
+    if (err.name === "TokenExpiredError") {
+      const message = "JSON Web Token is expired, Try again!!!";
+      error = new ErrorHandler(message, 400);
+    }
+
     res.status(error.statusCode).json({
       success: false,
       message: error.message || "Internal Server Error",
