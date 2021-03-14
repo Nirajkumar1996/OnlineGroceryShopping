5fce6fbec69128b5ab329a94a08088be41c7f30d
Nirajkumar Ablankar <ablankar.niraj@gmail.com>
2021-03-14 11:51:42 +0530

    basic header footer and home page created

---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+
+const Footer = () => {
+  return (
+    <Fragment>
+      <footer className="py-1">
+        <p className="text-center mt-1">
+          Fresh Mart - Online Grocery Shopping - 2020-2021, All Rights Reserved
+        </p>
+      </footer>
+    </Fragment>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/store.js
@@ -0,0 +1,1 @@
+import { createStore, combineReducers, appp } from "redux";
