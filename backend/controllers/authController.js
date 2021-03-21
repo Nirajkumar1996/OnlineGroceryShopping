d549ef4a042b20a6b4f959b16c27d447066ef138
Nirajkumar Ablankar <ablankar.niraj@gmail.com>
2021-03-21 07:25:24 +0530

    user commit

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,19 +6,25 @@ const sendToken = require("../utils/jwtToken");
 const sendEmail = require("../utils/sendEmail");
 
 const crypto = require("crypto");
-const { send } = require("process");
+const cloudinary = require("cloudinary");
 
 //* Register a user  => /api/v1/register
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
   const { name, email, password } = req.body;
+
+  const result = await cloudinary.v2.uploader.upload(req.body.avatar, {
+    folder: "avatars",
+    width: 150,
+    crop: "scale",
+  });
 
   const user = await User.create({
     name,
     email,
     password,
     avatar: {
-      public_id: "niraj1",
-      url: "url1",
+      public_id: result.public_id,
+      url: result.secure_url,
     },
   });
 
@@ -67,9 +73,7 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   //Create reset password url
-  const resetUrl = `${req.protocol}://${req.get(
-    "host"
-  )}/api/v1/password/reset/${resetToken}`;
+  const resetUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`;
 
   const message = `Your password reset token  is as follows:\n\n${resetUrl}\n\n
   If you have not requested this email, then ignore it`;
@@ -168,7 +172,24 @@ exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
     email: req.body.email,
   };
 
-  //*Update avatar : TODO
+  //*Update avatar
+  if (req.body.avatar !== "") {
+    const user = await User.findById(req.user.id);
+
+    const image_id = user.avatar.public_id;
+    const res = await cloudinary.v2.uploader.destroy(image_id);
+
+    const result = await cloudinary.v2.uploader.upload(req.body.avatar, {
+      folder: "avatars",
+      width: 150,
+      crop: "scale",
+    });
+
+    newUserData.avatar = {
+      public_id: result.public_id,
+      url: result.secure_url,
+    };
+  }
 
   const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
     new: true,
