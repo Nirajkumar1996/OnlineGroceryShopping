54842a0cf62c034303b4fc0cf8d490b5c7e22075
Nirajkumar Ablankar <ablankar.niraj@gmail.com>
2021-03-22 19:53:16 +0530

    cart module

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -13,15 +13,24 @@ import {
   forgotPasswordReducer,
 } from "./reducers/userReducers";
 
+import { cartReducers } from "./reducers/cartReducers";
+
 const reducer = combineReducers({
   products: productsReducer,
   productDetails: productDetailsReducer,
   auth: authReducer,
   user: userReducer,
   forgotPassword: forgotPasswordReducer,
+  cart: cartReducers,
 });
 
-let initialState = {};
+let initialState = {
+  cart: {
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [],
+  },
+};
 
 const middleware = [thunk];
 const store = createStore(
